b2ca213e8cc054339366bf980df39f1a3c1b8a98
Jing0717 <bleach81618@gmail.com>
2023-07-11 10:55:19 +0800

    [feat] Implement error handling and MongoDB connection

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+  globals: {
+    __dirname: 'readonly',
+    process: 'readonly',
+  },
+};
--- app.js
+++ app.js
@@ -3,15 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+require('./connections');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('*', cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,19 +26,53 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// express 錯誤處理:
+// *自己設定的 err 錯誤
+const resErrorProd = (err, res) => {
+  res.status(err.statusCode || 500).send({
+    status: err.statusCode,
+    name: err.name,
+    message: err.message,
+  });
+};
+// *開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+// 錯誤處理，一定要加上 next
+app.use(function (err, req, res, next) {
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  }
+  // production
+  if (err.name === 'ValidationError') {
+    err.message = '資料欄位未填寫正確，請重新輸入！';
+    err.isOperational = true;
+    return resErrorProd(err, res);
+  }
+  resErrorProd(err, res);
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 未捕捉到的 catch
+process.on('unhandledRejection', (err, promise) => {
+  console.error('未捕捉到的 rejection：', promise, '原因：', err);
+});
+
+// 程式出現重大錯誤時
+process.on('uncaughtException', (err) => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+  console.error('Uncaught Exception！');
+  console.error(err);
+  process.exit(1);
 });
 
 module.exports = app;
